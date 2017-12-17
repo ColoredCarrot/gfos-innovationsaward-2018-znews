5da09b73f4bc89b63a7d45b6a0859991b1976362
ColoredCarrot <contact.coloredcarrot@gmail.com>
2017-12-17 19:12:41 +0100

    Rework GetNewslettersResource, add 'include-non-published'-option
    The returned data now also contains, per newsletter, 'published',
    if 'include-non-published' was specified.
    
    Authentication is required if either 'include-non-published' or
    'includenid' (or both) is specified.

--- ZNews Server/src/de/znews/server/resources/GetNewslettersResource.java
+++ ZNews Server/src/de/znews/server/resources/GetNewslettersResource.java
@@ -7,13 +7,16 @@ import de.znews.server.ZNews;
 import de.znews.server.newsletter.Newsletter;
 import de.znews.server.resources.exception.HttpException;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Locale;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class GetNewslettersResource extends JSONResource
 {
+    
+    private static final List<String> TRUE_STRINGS = Collections.unmodifiableList(Arrays.asList("true", "1", "ok", "yes"));
     
     public GetNewslettersResource(ZNews znews)
     {
@@ -24,40 +27,40 @@ public class GetNewslettersResource extends JSONResource
     protected JsonNode handleJsonRequest(RequestContext ctx) throws HttpException
     {
         
+        boolean includenid          = ctx.hasParam("includenid") && TRUE_STRINGS.contains(ctx.getStringParam("includenid").toLowerCase(Locale.ENGLISH));
+        boolean includeNonPublished = ctx.hasParam("include-non-published") && TRUE_STRINGS.contains(ctx.getStringParam("include-non-published").toLowerCase(Locale.ENGLISH));
+        
+        if (includenid || includeNonPublished)
+            znews.authenticator.requireAuthentication(ctx);
+        
         int amount = ctx.getIntParam("amount", 5);
         
-        Iterable<Newsletter> latestNewsletters = znews.newsletterManager.getLatestNewsletters(amount);
+        Stream<Newsletter> newsletterStream = znews.newsletterManager.getLatestNewsletters()
+                                                                     .limit(amount);
         
-        // Include ids only if logged in and `includenid` parameter is specified
-        List<JsonNode> dataAsList = (ctx.hasParam("includenid") ? dataWithIds(latestNewsletters, ctx) : dataWithoutIds(latestNewsletters))
-                .collect(Collectors.toList());
+        if (!includeNonPublished)
+            newsletterStream = newsletterStream.filter(Newsletter::isPublished);
+        
+        Stream<JsonObject.Builder> stream = includenid ? newsletterStream.map(n -> JsonObject.createBuilder()
+                                                                                             .add("title", n.getTitle())
+                                                                                             .add("text", n.getText())
+                                                                                             .add("published", n.isPublished())
+                                                                                             .add("nid", n.getId()))
+                                                       : newsletterStream.map(n -> JsonObject.createBuilder()
+                                                                                             .add("title", n.getTitle())
+                                                                                             .add("text", n.getText())
+                                                                                             .add("published", n.isPublished()));
+        
+        if (!includeNonPublished)
+            stream = stream.map(n -> n.remove("published"));
         
         JsonArray.Builder data = JsonArray.createBuilder();
         
-        dataAsList.forEach(data::add);
+        stream.map(JsonObject.Builder::build)
+              .forEach(data::add);
         
         return JsonObject.createBuilder().add("success", true).add("data", data.build()).build();
         
-    }
-    
-    private Stream<JsonNode> dataWithIds(Iterable<Newsletter> newsletters, RequestContext ctx) throws HttpException
-    {
-        znews.authenticator.requireAuthentication(ctx);
-        return StreamSupport.stream(newsletters.spliterator(), false)
-                            .map(n -> JsonObject.createBuilder()
-                                                .add("title", n.getTitle())
-                                                .add("text", n.getText())
-                                                .add("nid", n.getId())
-                                                .build());
-    }
-    
-    private Stream<JsonNode> dataWithoutIds(Iterable<Newsletter> newsletters)
-    {
-        return StreamSupport.stream(newsletters.spliterator(), false)
-                            .map(n -> JsonObject.createBuilder()
-                                                .add("title", n.getTitle())
-                                                .add("text", n.getText())
-                                                .build());
     }
     
 }
