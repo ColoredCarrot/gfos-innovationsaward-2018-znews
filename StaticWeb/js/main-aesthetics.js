d1d9ab6b8930a94dd1f6c189726517403208b7aa
ColoredCarrot <contact.coloredcarrot@gmail.com>
2017-12-19 22:41:19 +0100

    #14: Make ZNEWS logo clickable to redirect to /

--- StaticWeb/js/main-aesthetics.js
+++ StaticWeb/js/main-aesthetics.js
@@ -3,4 +3,6 @@ jQuery(function($)
     let onResize = () => $('div.background').css('height', window.innerHeight + 'px');
     $(window).resize(onResize);
     onResize();
+
+    $('img[src="/img/logo.jpg"]').click(() => window.location.href = '/');
 });
