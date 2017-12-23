f2a3e5c1f4712c7ff71ec3049a49372b62a1ded4
ColoredCarrot <contact.coloredcarrot@gmail.com>
2017-12-23 15:34:47 +0100

    Beautify footer (add change password, logout links + arrow bullets)

--- StaticWeb/js/auto-add-main-tag.js
+++ StaticWeb/js/auto-add-main-tag.js
@@ -4,38 +4,54 @@ jQuery(function($)
     {
         // FIXME: Duplicate code (in /js/main-aesthetics.js)
         $('body').children().appendTo($('<main></main>').appendTo($('body')));
-        $('body').append($('<footer class="page-footer">\n' +
-            '    <div class="container">\n' +
-            '        <div class="row">\n' +
-            '            <div class="col l6 s12">\n' +
-            '                <h5 class="white-text">ZNews Project</h5>\n' +
-            '                <p class="grey-text text-lighten-4">\n' +
-            '                    The ZNews Project was created by Julian Bennet Koch\n' +
-            '                    and Mats Holtbecker to be entered into the GFOS\n' +
-            '                    Innovationsaward 2018 contest. It, entailing any\n' +
-            '                    related components that are not owned by another party,\n' +
-            '                    including (but not limited to) the source code and/or\n' +
-            '                    binary files of the entire website and the server,\n' +
-            '                    is not to be used by a third party, profit or non-profit,\n' +
-            '                    nor be published, distributed, or in any way made available\n' +
-            '                    to anyone for any reason without express, written permission\n' +
-            '                    from one of the authors.\n' +
-            '                </p>\n' +
-            '            </div>\n' +
-            '            <div class="col l4 offset-l2 s12">\n' +
-            '                <h5 class="white-text">Navigation</h5>\n' +
-            '                <ul>\n' +
-            '                    <li><a class="grey-text text-lighten-3" href="/">Main Page</a></li>\n' +
-            '                    <li><a class="grey-text text-lighten-3" href="/admin/dashboard">Administrator Login</a></li>\n' +
-            '                </ul>\n' +
-            '            </div>\n' +
-            '        </div>\n' +
-            '    </div>\n' +
-            '    <div class="footer-copyright">\n' +
-            '        <div class="container">\n' +
-            '            © 2018 Julian Koch, Mats Holtbecker ALL RIGHTS RESERVED\n' +
-            '        </div>\n' +
-            '    </div>\n' +
-            '</footer>'));
+        $('body').append($(`<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">ZNews Project</h5>
+                <p class="grey-text text-lighten-4">
+                    The ZNews Project was created by Julian Bennet Koch
+                    and Mats Holtbecker to be entered into the GFOS
+                    Innovationsaward 2018 contest. It, entailing any
+                    related components that are not owned by another party,
+                    including (but not limited to) the source code and/or
+                    binary files of the entire website and the server,
+                    is not to be used by a third party, profit or non-profit,
+                    nor be published, distributed, or in any way made available
+                    to anyone for any reason without express, written permission
+                    from one of the authors.
+                </p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Navigation</h5>
+                <ul id="nav-links">
+                    <li>
+                        <a class="grey-text text-lighten-3" href="/">Main Page</a>
+                        <i class="material-icons left nav-link-bullet">keyboard_arrow_right</i>
+                    </li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="/admin/dashboard">Administrator Login</a>
+                        <i class="material-icons left nav-link-bullet">keyboard_arrow_right</i>
+                    </li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="/admin/logout">Logout</a>
+                        <i class="material-icons left nav-link-bullet nav-link-bullet-indent">keyboard_arrow_right</i>
+                    </li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="/admin/change_password">Change Password</a>
+                        <i class="material-icons left nav-link-bullet">keyboard_arrow_right</i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2018 Julian Koch, Mats Holtbecker ALL RIGHTS RESERVED
+        </div>
+    </div>
+</footer>
+`));
+
     }
 });
