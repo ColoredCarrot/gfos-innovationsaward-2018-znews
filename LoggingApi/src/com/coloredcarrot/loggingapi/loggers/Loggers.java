20142a1cbd009343730720d62c4795373cf921d6
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-01-12 20:49:23 +0100

    Add Loggers#build(Properties) to build Logger from config

--- LoggingApi/src/com/coloredcarrot/loggingapi/loggers/Loggers.java
+++ LoggingApi/src/com/coloredcarrot/loggingapi/loggers/Loggers.java
@@ -2,7 +2,17 @@ package com.coloredcarrot.loggingapi.loggers;
 
 import com.coloredcarrot.loggingapi.LogRecord;
 
+import java.io.File;
 import java.io.Writer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
 import java.util.function.Function;
 
 public interface Loggers
@@ -11,6 +21,47 @@ public interface Loggers
     static LoggerBuilder builder()
     {
         return new LoggerBuilder();
+    }
+    
+    static Logger build(Properties props)
+    {
+        LoggerBuilder b = builder()
+                .filter(LogRecord.Level.valueOf(props.getProperty("log.filter", "out").toUpperCase(Locale.ENGLISH)));
+        
+        Map<LogRecord.Level, Map.Entry<String, DateFormat>> formats = new HashMap<>();
+        for (Map.Entry<Object, Object> e : props.entrySet())
+        {
+            String key = String.valueOf(e.getKey());
+            if (key.startsWith("log.format."))
+            {
+                LogRecord.Level level = LogRecord.Level.valueOf(key.substring("log.format.".length() + 1, key.endsWith(".date-format") ? key.length() - ".date-format".length() : key.length()).toUpperCase(Locale.ENGLISH));
+                if (key.endsWith(".date-format"))
+                {
+                    DateFormat newDateFormat = new SimpleDateFormat(String.valueOf(e.getValue()));
+                    formats.compute(level, (l, old) -> old == null ? new AbstractMap.SimpleImmutableEntry<>(null, newDateFormat) : new AbstractMap.SimpleImmutableEntry<>(old.getKey(), newDateFormat));
+                }
+                else
+                {
+                    String newFormat = String.valueOf(e.getValue());
+                    formats.compute(level, (l, old) -> old == null ? new AbstractMap.SimpleImmutableEntry<>(newFormat, null) : new AbstractMap.SimpleImmutableEntry<>(newFormat, old.getValue()));
+                }
+            }
+        }
+        
+        List<Object[]> list = new ArrayList<>();
+        for (Map.Entry<LogRecord.Level, Map.Entry<String, DateFormat>> e : formats.entrySet())
+            list.add(new Object[] { e.getKey(), e.getValue().getKey(), e.getValue().getValue() });
+        Object[][] formatsArray = list.toArray(new Object[0][0]);
+    
+        b.format(formatsArray);
+        
+        if (Boolean.parseBoolean(props.getProperty("log.separate-thread", "true")))
+            b.newPhase(new NewThreadLogger(null));
+    
+        // Right now, only logging to a directory using gzip is supported
+        File logDir = new File(props.getProperty("log.out.files-gzip.dir", "log/"));
+        return b.saveInto(new FilesGzipLogger(logDir, Boolean.parseBoolean(props.getProperty("log.out.files-gzip.auto-flush", "true"))));
+        
     }
     
     static Logger trash()
