2a1f0e7d9788f6e8a3ee73984d07212297bcf513
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-01-12 21:06:51 +0100

    Update TestMain

--- LoggingApi/src/com/coloredcarrot/loggingapi/TestMain.java
+++ LoggingApi/src/com/coloredcarrot/loggingapi/TestMain.java
@@ -1,7 +1,9 @@
 package com.coloredcarrot.loggingapi;
 
+import com.coloredcarrot.loggingapi.loggers.FilesGzipLogger;
 import com.coloredcarrot.loggingapi.loggers.Logger;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 
 public class TestMain
@@ -10,20 +12,35 @@ public class TestMain
     public static void main(String[] args)
     {
         Logger logger = Logger.builder()
-                              //.filter(LogRecord.Level.OUT)
+                              .filter(LogRecord.Level.OUT)
+                              .delegateToOtherThreads()
                               .format(new Object[][] {
                                       { LogRecord.Level.DEV, "[{date}] [{threadName}:{threadId}/{level} @ {methodName}/{typeSimpleName}/{fileName}:{line}] {msg}", new SimpleDateFormat("HH:mm:ss:SSS") },
                                       { LogRecord.Level.DEBUG, "[{date}] [{threadName}:{threadId}/{level}] {msg}" },
                                       { LogRecord.Level.FATAL, "FATAL [{date}] [{threadName}:{threadId}/{level} @ {methodName}/{typeSimpleName}/{fileName}:{line}] {msg}" }
                               })
-                              .saveInto(System.out);
-                              //.build();
-    
+                              .copyInto(System.out)
+                              .saveInto(new FilesGzipLogger(new File("the_mf_logs/"), true));
+        //.build();
+        
         logger.dev("my dev");
         logger.debug("my debug");
         logger.out("my out");
         logger.err("my err", new RuntimeException("Hello, ", new NullPointerException("world!")));
         logger.fatal("my fatal");
+        
+        new Thread(() ->
+        {
+            try
+            {
+                Thread.sleep(3000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            System.exit(0);
+        }).start();
         
     }
     
