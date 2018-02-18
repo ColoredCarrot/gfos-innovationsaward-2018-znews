14def8bd083fc381bf73092834655e8f3788650e
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-02-18 19:39:41 +0100

    Add subscribedTags: Set<String> property to Registration

--- ZNews Server/src/de/znews/server/newsletter/Registration.java
+++ ZNews Server/src/de/znews/server/newsletter/Registration.java
@@ -1,27 +1,32 @@
 package de.znews.server.newsletter;
 
 import com.coloredcarrot.jsonapi.reflect.JsonSerializable;
+import de.znews.server.ZNews;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 public class Registration implements Serializable, JsonSerializable
 {
-	
-	private static final long serialVersionUID = -8221233446973804554L;
-	
-	protected static Registration newStandardRegistration(String email)
-	{
-		Registration reg = new Registration();
-		reg.email = email;
-		return reg;
-	}
-	
-	private String email;
-	
-	private Registration()
-	{
-	}
+    
+    private static final long serialVersionUID = -3977523515006038071L;
+    
+    protected static Registration newStandardRegistration(ZNews znews, String email)
+    {
+        Registration reg = new Registration();
+        reg.email = email;
+        reg.subscribedTags.addAll(znews.tagsList.getTags());
+        return reg;
+    }
+    
+    private String email;
+    private Set<String> subscribedTags = new HashSet<>();
+    
+    private Registration()
+    {
+    }
     
 }
