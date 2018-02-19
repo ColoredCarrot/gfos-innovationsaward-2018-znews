e82e9b082a9e3aeb1c113592c81d0cb14ae638e4
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-02-19 21:45:21 +0100

    Implement EditSubscriptionDataResource (requires testing)

--- ZNews Server/src/de/znews/server/resources/EditSubscriptionDataResource.java
+++ ZNews Server/src/de/znews/server/resources/EditSubscriptionDataResource.java
@@ -1,9 +1,18 @@
 package de.znews.server.resources;
 
+import com.coloredcarrot.jsonapi.Json;
+import com.coloredcarrot.jsonapi.ast.JsonArray;
 import com.coloredcarrot.jsonapi.ast.JsonNode;
-import com.coloredcarrot.jsonapi.ast.JsonObject;
+import com.coloredcarrot.jsonapi.parsing.JsonException;
 import de.znews.server.ZNews;
+import de.znews.server.newsletter.Registration;
+import de.znews.server.resources.exception.Http400BadRequestException;
 import de.znews.server.resources.exception.HttpException;
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+import java.io.StringReader;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class EditSubscriptionDataResource extends JSONResource
 {
@@ -16,14 +25,50 @@ public class EditSubscriptionDataResource extends JSONResource
     @Override
     public JsonNode handleJsonRequest(RequestContext ctx) throws HttpException
     {
-    
-        ctx.getStringParam("email");
-        // TODO: Get data for email
-    
-        return JsonObject.createBuilder()
-                         .add("success", true)
-                         .build();
-    
+        
+        String email = ctx.getStringParam("email");
+        
+        Registration reg = znews.registrationList.getRegistration(email);
+        
+        if (reg == null)
+            throw new HttpException(HttpResponseStatus.NOT_FOUND, email);
+        
+        // Currently, we don't support other data types but "application/x-www-form-urlencoded";
+        // we must use some other data serialization format in the parameter values
+        if (ctx.hasPostParam("update_subscribed_tags"))
+        {
+            
+            JsonArray newTags;
+            try
+            {
+                newTags = (JsonArray) Json.getInputStream(new StringReader(ctx.getStringPostParam("update_subscribed_tags")))
+                                          .next();
+            }
+            catch (JsonException | ClassCastException e)
+            {
+                throw new Http400BadRequestException("Malformed or invalid JSON (" + e.getMessage() + ")");
+            }
+            
+            // Update subscribed-to tags.
+            // All JSON contents (of the array) are converted to String.
+            // We may want to check whether they are any other type and then
+            //  throw an Http400BadRequestException.
+            reg.setSubscribedTags(new HashSet<>(
+                    newTags.getContents().stream()
+                           .map(JsonNode::stringValue)
+                           .collect(Collectors.toSet())
+            ));
+            
+            // Don't cancel method execution so the client has a chance to
+            // review the results (NYI).
+            
+        }
+        
+        // Just return all subscribed-to tags
+        return JsonArray.createBuilder()
+                        .addAll(reg.getAllSubscribedTags(znews))
+                        .build();
+        
     }
     
 }
