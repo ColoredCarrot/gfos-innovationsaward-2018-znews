39ebb6974a0a7812eac1b07affd6b0802943c53a
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-02-25 20:20:17 +0100

    Configure Quill to allow formula + code highlighting, add more buttons
    To do this, these dependencies are added:
     - KaTeX
     - highlight.js
    (TODO: add licenses)

--- StaticWeb/editor/editor.js
+++ StaticWeb/editor/editor.js
@@ -1,8 +1,23 @@
 jQuery(function($)
 {
 
+    let toolbarStructure = [
+        [ { 'size': [ 'small', 'normal', 'large', 'huge' ] } ],
+        [ 'bold', 'italic', 'underline', 'strike' ],
+        [ { 'header': 1 }, { 'header': 2 }, 'blockquote', 'code-block', { 'list': 'ordered' }, { 'list': 'bullet' } ],
+        [ 'link', 'image', 'video', 'formula' ],
+        [ { 'indent': '-1' }, { 'indent': '+1' }, { 'align': [] } ],
+        [ 'clean' ]
+    ];
+
     let quill = new Quill('#editor', {
-        theme: 'snow'
+        theme: 'snow',
+        modules: {
+            toolbar: toolbarStructure,
+            syntax: true,
+            formula: true,
+            clipboard: true
+        }
     });
 
 });
