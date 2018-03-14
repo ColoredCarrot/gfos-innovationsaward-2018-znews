63437a5bee18a9784ef6d478b97a383e8f959483
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-03-14 18:03:13 +0100

    Include core quill stylesheet in /view, add ql-editor class to container

--- StaticWeb/js/delta_renderer.js
+++ StaticWeb/js/delta_renderer.js
@@ -1,7 +1,7 @@
 var DeltaRenderer = (function(dr)
 {
 
-    let virtContainer = $('<div/>').appendTo($('<div/>'))[0];  // Must wrap in two divs because of third-party bug (e.g. https://github.com/angular/angular.js/issues/13174)
+    let virtContainer = $('<div class="ql-editor"/>').appendTo($('<div/>'))[0];  // Must wrap in two divs because of third-party bug (e.g. https://github.com/angular/angular.js/issues/13174)
     let quill = new Quill(virtContainer, {
         modules: dr.options.loadModules
     });
