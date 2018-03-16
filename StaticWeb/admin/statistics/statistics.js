eceda0df974c9885c19c1aedac80a6000f01ecb4
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-03-16 19:34:53 +0100

    Implement (currently static) statistics html generation

--- StaticWeb/admin/statistics/statistics.js
+++ StaticWeb/admin/statistics/statistics.js
@@ -1,7 +1,18 @@
 jQuery(function($)
 {
 
-
+    setColumns([{
+        title: "Registrations",
+        domId: "col-regs",
+        entries: [
+            {
+                title: "contact.coloredcarrot@gmail.com",
+                attrs: {
+                    "Successfully sent emails": "12/13"
+                }
+            }
+        ]
+    }]);
 
 });
 
@@ -10,16 +21,20 @@ function setColumns(columns = [])
 
     /*
     column.
-        title: string
-        attrs: { attrName1: attrVal1, attrName2: attrVal2 }
         domId: string
+        title: string
+        entries: [
+            entry.
+                title: string
+                attrs: { attrName1: attrVal1, attrName2: attrVal2 }
+        ]
         callbacks.
             insert: function($column, column)    // pre  column insertion
             inserted: function($column, column)  // post column insertion
 
      */
 
-    let COLUMN_TEMPLATE_VARIABLES = {
+    let ENTRY_TEMPLATE_VARIABLES = {
         '{{TITLE}}': column => column.title,
         '{{ATTRS}}': column => Object.keys(column.attrs).reduce((res, attrName) => res + `<span>${attrName}: ${column.attrs[attrName]}</span>`, '')
     };
@@ -27,46 +42,40 @@ function setColumns(columns = [])
     if (columns.length > 4)
         throw new Error(`number of columns (${columns.length}) exceeds maximum (4)`);
 
-    loadColumnTemplate().then(function success(data)
+    let $columnsContainer = $('#general-stats-container');
+    let numColumns = columns.length;
+    let columnWidth = 12 / numColumns;
+
+    $columnsContainer.empty();
+
+    columns.forEach((column, idx) =>
     {
-        useColumnTemplate($(data));
+
+        let $column = $('<ul class="collection with-header"></ul>');
+
+        $column.append($(`<li class="collection-header"><h5>${column.title}</h5></li>`));
+
+        let entryTemplate = '<li class="collection-item"><span class="title">{{TITLE}}</span><p>{{ATTRS}}</p></li>';
+
+        column.entries
+              .map(entry => Object.keys(ENTRY_TEMPLATE_VARIABLES)
+                                  .map(key => [key, ENTRY_TEMPLATE_VARIABLES[key]])
+                                  .map(e => [e[0], e[1](entry)])
+                                  .reduce((res, e) => res.replace(e[0], e[1]), entryTemplate))
+              .map(html => $(html))
+              .forEach($e => $column.append($e));
+
+        let $wrappedColumn = $(`<div class="col s${(idx + 1) * columnWidth}" id="${column.domId}"></div>`)
+            .append($column);
+
+        if (column.callbacks && column.callbacks.insert)
+            column.callbacks.insert($column, column);
+
+        $columnsContainer.append($wrappedColumn);
+
+        if (column.callbacks && column.callbacks.inserted)
+            column.callbacks.inserted($column, column);
+
     });
-
-    function useColumnTemplate(columnTemplate)
-    {
-        let $columnsContainer = $('#general-stats-container');
-        let numColumns = columns.length;
-        let columnWidth = 12 / numColumns;
-
-        $columnsContainer.empty();
-
-        columns.forEach((column, idx) =>
-        {
-
-            let $column = $($.map(COLUMN_TEMPLATE_VARIABLES, (val, key) => [key, val])
-                             .map(e => [e[0], e[1](column)])
-                             .reduce((res, e) => res.replace(e[0], e[1]), columnTemplate));
-
-            let $wrappedColumn = $(`<div class="col s${(idx + 1) * columnWidth}" id="${column.domId}"></div>`)
-                .append($column);
-
-            if (column.callbacks && column.callbacks.insert)
-                column.callbacks.insert($column, column);
-
-            $columnsContainer.append($wrappedColumn);
-
-            if (column.callbacks && column.callbacks.inserted)
-                column.callbacks.inserted($column, column);
-
-        });
-    }
-
-    function loadColumnTemplate()
-    {
-        return $.ajax('/statistics/column.html', {
-            dataType: 'text',
-            cache: true
-        });
-    }
 
 }
