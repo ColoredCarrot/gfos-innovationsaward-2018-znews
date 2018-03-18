30eee9b490e7ddb7b416f6062da6d8c472add46f
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-03-18 10:53:14 +0100

    Add html version of double opt-in email

--- ZNews Server/src/de/znews/server/emai_reg/EmailTemplates.java
+++ ZNews Server/src/de/znews/server/emai_reg/EmailTemplates.java
@@ -12,13 +12,14 @@ public class EmailTemplates
     
     public EmailTemplates(ZNews znews)
     {
-        DOUBLE_OPT_IN = new FileEmailTemplate(znews, "/resources/email_templates/double_opt_in/double_opt_in.plaintext.txt", null, new File(znews.config.getEmailConfig()
-                                                                                                                                                        .getTemplatePathDoubleOptIn()), /*FIXME: Make email subject configurable*/"Confirm Subscription");
+        DOUBLE_OPT_IN = new FileEmailTemplate(znews,
+                "/resources/email_templates/double_opt_in/double_opt_in.plaintext.txt",
+                "/resources/email_templates/double_opt_in/double_opt_in.html.html",
+                new File(znews.config.getEmailConfig().getTemplatePathDoubleOptIn()), /*FIXME: Make email subject configurable*/"Confirm Subscription");
         NEW_NEWSLETTER = new FileEmailTemplate(znews,
                 "/resources/email_templates/new_newsletter/new_newsletter.plaintext.txt",
                 "/resources/email_templates/new_newsletter/new_newsletter.html.html",
-                new File(znews.config.getEmailConfig()
-                                     .getTemplatePathNewNewsletter()), /*FIXME: Make email subject configurable*/"New Newsletter");
+                new File(znews.config.getEmailConfig().getTemplatePathNewNewsletter()), /*FIXME: Make email subject configurable*/"New Newsletter");
     }
     
 }
