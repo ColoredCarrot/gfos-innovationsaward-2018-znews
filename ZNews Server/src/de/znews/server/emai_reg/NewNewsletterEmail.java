039398dfdf2c9f929eb8cc33cbff7ae6bd2282f3
ColoredCarrot <contact.coloredcarrot@gmail.com>
2018-03-18 10:34:50 +0100

    "Fix" email templates

--- ZNews Server/src/de/znews/server/emai_reg/NewNewsletterEmail.java
+++ ZNews Server/src/de/znews/server/emai_reg/NewNewsletterEmail.java
@@ -30,6 +30,7 @@ public class NewNewsletterEmail extends Email
     {
         return new Object[] {
                 "{{title}}", title,
+                "{{full_address}}", znews.config.getFullExternalAddress(),
                 "{{registered_email_name}}", registeredEmail.substring(0, registeredEmail.indexOf('@')),
                 "{{registered_email}}", registeredEmail,
                 "{{nid}}", nid
